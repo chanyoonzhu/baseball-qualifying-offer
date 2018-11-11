94127486b246f54caee7d146754e548b5262e68d
Your Name <zhuchanyoon@gmail.com>
2018-11-11 17:11:46 -0600

    fix indentation and wording

--- static/js/data.js
+++ static/js/data.js
@@ -22,18 +22,18 @@ $(function() {
         google.charts.load("current", {packages:["corechart"]});
         google.charts.setOnLoadCallback(drawChart1);
         function drawChart1() {
-            // params
+            
             var year = salaryObj['year']
             var level = salaryObj['level']
 
             var data = google.visualization.arrayToDataTable(salaryObj['table']);
 
             var options = {
-            title: year + ' ' + level + ' Salaries',
-            titleTextStyle: {color: '#E81828'},
-            legend: { position: 'none' },
-            color: '#284898',
-            hAxis: { gridlines: {color: '#E81828'}},
+                title: year + ' ' + level + ' Salaries',
+                titleTextStyle: {color: '#E81828'},
+                legend: { position: 'none' },
+                color: '#284898',
+                hAxis: { gridlines: {color: '#E81828'}},
             };
 
             var chart = new google.visualization.Histogram(document.getElementById('chart1_div'));
@@ -42,18 +42,18 @@ $(function() {
 
         google.charts.setOnLoadCallback(drawChart2);
         function drawChart2() {
-            // params
+            
             var year = salaryObj['year']
             var level = salaryObj['level']
 
             var data = google.visualization.arrayToDataTable(salaryObj['best']);
 
             var options = {
-            title: year + ' ' + level + ' Top ' + (salaryObj['best'].length - 1) + ' Salaries',
-            titleTextStyle: {color: '#E81828'},
-            hAxis: { textPosition: 'none' },
-            legend: { position: 'none' },
-            color: '#284898',
+                title: year + ' ' + level + (salaryObj['best'].length - 1) + ' Highest ' + ' Salaries',
+                titleTextStyle: {color: '#E81828'},
+                hAxis: { textPosition: 'none' },
+                legend: { position: 'none' },
+                color: '#284898',
             };
 
             var chart = new google.visualization.LineChart(document.getElementById('chart2_div'));
@@ -67,10 +67,10 @@ $(function() {
             var data = google.visualization.arrayToDataTable(neighbors);
 
             var options = {
-            title: 'Salaries Close to Qualifying Offer',
-            titleTextStyle: {color: '#E81828'},
-            legend: { position: 'none' },
-            color: '#284898',
+                title: 'Salaries Close to Qualifying Offer',
+                titleTextStyle: {color: '#E81828'},
+                legend: { position: 'none' },
+                color: '#284898',
             };
 
             var chart = new google.visualization.BarChart(document.getElementById('chart3_div'));
